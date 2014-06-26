84485e5284d7a5ff4381b5adf656a4b53672b817
Ashley <ashleykwtam@gmail.com>
2014-06-25 22:58:35 -0400

    created a customer model, controller, and sign-up view

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,19 @@
+class CustomersController < ApplicationController
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(user_params)
+    if @customer.save
+      redirect_to products_url, :notice => "Signed up!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :password, :password_confirmation, :bio_description)
+  end
+end
